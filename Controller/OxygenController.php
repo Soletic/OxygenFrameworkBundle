36dab0181b98da7c2ca2e9d6a5ff162d379df74a
Laurent Chedanne <laurent@chedanne.pro>
2013-07-14 03:04:23 +0200

    Raccourci translate dans OxygenController Flash message avec fonction twig : oxygen_flash_messages

--- Controller/OxygenController.php
+++ Controller/OxygenController.php
@@ -221,4 +221,31 @@ class OxygenController extends ContainerAware
     {
         return $this->container->get($id);
     }
+
+
+    /**
+     * Creates and returns a Form instance from the type of the form.
+     *
+     * @param string|FormTypeInterface $type    The built type of the form
+     * @param mixed                    $data    The initial data for the form
+     * @param array                    $options Options for the form
+     *
+     * @return Form
+     */
+    public function createForm($type, $data = null, array $options = array())
+    {
+    	return $this->container->get('form.factory')->create($type, $data, $options);
+    }
+    
+    /**
+     * Translate a string
+     * 
+     * @param string $message
+     * @param array $params
+     * @param string $domain
+     */
+    public function translate($message, $params = array(), $domain = null) {
+    	return $this->get('translator')->trans($message, $params, $domain);
+    }
+
 }
---
+++ Templating/FlashMessagesTwigExtension.php
@@ -0,0 +1,74 @@
+<?php
+namespace Oxygen\FrameworkBundle\Templating;
+
+/**
+ * Container de messages flash
+ * 
+ * @author lolozere
+ *
+ */
+class FlashMessagesTwigExtension extends \Twig_Extension {
+	
+	protected $session;
+	/**
+	 * @var \Twig_Environment
+	 */
+	protected $environment;
+	
+	public function __construct($session) {
+		$this->session = $session;
+	}
+	
+	public function initRuntime(\Twig_Environment $environment)
+	{
+		$this->environment = $environment;
+	}
+	
+	/**
+	 * Déclaration des fonctions étendant Twig
+	 *
+	 * @see Twig_Extension::getFunctions()
+	 */
+	public function getFunctions() {
+		return array(
+				'oxygen_flash_messages'          => new \Twig_Function_Method($this, 'flashMessages'),
+		);
+	}
+	
+	/**
+	 * Retourne le nom de l'extension
+	 *
+	 * @see Twig_ExtensionInterface::getName()
+	 */
+	public function getName()
+	{
+		return 'oxygen_framework_flash_messages';
+	}
+	
+	public function addError($message) {
+		$this->session->getFlashBag()->add('error', $message);
+	}
+	
+	public function addWarning($message) {
+		$this->session->getFlashBag()->add('warning', $message);
+	}
+	
+	public function addSuccess($message) {
+		$this->session->getFlashBag()->add('success', $message);
+	}
+	
+	public function addInfo($message) {
+		$this->session->getFlashBag()->add('info', $message);
+	}
+	
+	/**
+	 * Return HTML of flash messages according to OxygenFrameworkBundle::flash_messages.html.twig
+	 * 
+	 * @return string
+	 */
+	public function flashMessages() {
+		$template = $this->environment->loadTemplate('OxygenFrameworkBundle::flash_messages.html.twig');
+		return $template->renderBlock('flash_messages', $this->environment->getGlobals());
+	}
+	
+}
